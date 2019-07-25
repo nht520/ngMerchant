6cf8c2febf374202177649741c388b06e8d6df26
nht520 <1278800400@qq.com>
2019-07-25 22:46:48 +0800

    分页

--- src/app/components/historystatistics/historystatistics.component.ts
+++ src/app/components/historystatistics/historystatistics.component.ts
@@ -14,6 +14,13 @@ export class HistorystatisticsComponent implements OnInit {
   list: any[] = [];
   timeone:any;
   timetwo:any;
+
+  current = 1;
+  size = 10;
+  total:any;
+  model = 1;
+  pages:any;
+
   constructor( public storage:StorageService, public busurl:BesurlService) { }
   usertext(){
     this.text = this.storage.get("navlist");
@@ -23,21 +30,33 @@ export class HistorystatisticsComponent implements OnInit {
     this.userlist();
   }
   // 列表
-  userlist(){
+  userlist(e){
     const api = this.busurl.window.daystatistics;
     var param = {
       params:{
         channelId:this.storage.get("user").id,
+        current:e,
       }
     };
     Axios.get(api,param).then((res)=>{
       console.log(res)
       this.list=res.data.records;
+
+      this.current = res.data.pages;
+      this.size = res.data.size;
+      this.total = res.data.total;
+      this.model = res.data.current;
+      
     }).catch((err)=>{
       console.log(err)
     })
   }
 
+  nav(e){
+    this.pages = e;
+    this.userlist(this.pages);
+  }
+  
   handle(time: number) {
     // [time] is string
     console.log(time)
